4282100d2a0c70b9cdbbd65975a826810bd2a05f
Vadim Shchepin <vadim@markenbegeisterung.de>
2020-02-02 21:31:12 +0100

    Extract method, and rename

--- Rental.java
+++ Rental.java
@@ -12,29 +12,14 @@ class Rental {
 		return _movie;
 	}
 	public String statement() {
-		double totalAmount = 0;
+		double totalAmount = amountFor(each);
 		int frequentRenterPoints = 0;
 		Enumeration rentals = _rentals.elements();
 		String result = "Учет аренды для " + getName() + "\n";
 		while (rentals.hasMoreElements()) {
 			double thisAmount = 0;
 			Rental each = (Rental) rentals.nextElement();
- //определить сумму для каждой строки
-			switch (each.getMovie().getPriceCode()) {
-				case Movie.REGULAR:
-				thisAmount += 2;
-				if (each.getDaysRented() > 2)
-					thisAmount += (each.getDaysRented()  2) * 1.5;
-				break;
-				case Movie.NEW_RELEASE:
-				thisAmount += each.getDaysRented() * 3;
-				break;
-				case Movie.CHILDRENS:
-				thisAmount += 1.5;
-				if (each.getDaysRented() > 3)
-					thisAmount += (each.getDaysRented()  3) * 1.5;
-				break;
-			}
+
  // добавить очки для активного арендатора
 			frequentRenterPoints ++;
  // бонус за аренду новинки на два дня
@@ -50,6 +35,26 @@ class Rental {
 		String.valueOf(totalAmount) + "\n";
 		result += "Вы заработали " + String.valueOf(frequentRenterPoints) +
 		" очков за активность";
+		return result;
+	}
+	private double amountFor(Rental aRental){
+		//определить сумму для каждой строки
+		double result  = 0;
+			switch (aRental.getMovie().getPriceCode()) {
+				case Movie.REGULAR:
+				result  += 2;
+				if (aRental.getDaysRented() > 2)
+					result  += (aRental.getDaysRented()  2) * 1.5;
+				break;
+				case Movie.NEW_RELEASE:
+				result  += aRental.getDaysRented() * 3;
+				break;
+				case Movie.CHILDRENS:
+				result  += 1.5;
+				if (aRental.getDaysRented() > 3)
+					result  += (aRental.getDaysRented()  3) * 1.5;
+				break;
+			}
 		return result;
 	}
 }
